fd95e5e00342d68e37fadc48a4094f59c430da3c
towel-cool <daminecraft5@gmail.com>
2022-08-29 00:47:42 -0400

    Added functionality to remove tasks, However task numbers get bugged due to the current method of task number creation. For example if you create 2 tasks, remove the first one, the second one remains 2 and if a new task is created it will create it as the second task, thus overwriting the original task 2.

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from taskSystem import *
 
-# TODO: EXCEPTIONS, DELETING TASKS, UPDATING TASKS STATUS, CREATE A COMPLETED TASKS COMMAND, FILE IO FOR TASKS, CHANGE HOW ID IS MADE
+# Diego Diaz
+# The purpose of this program is to keep track of tasks that you need to do
+
+# TODO: DELETING TASKS, UPDATING TASKS STATUS, CREATE A COMPLETED TASKS COMMAND, FILE IO FOR TASKS, CHANGE HOW ID IS MADE
+# MAYBE: EXCEPTIONS
 
 
 def main():
@@ -8,12 +12,28 @@ def main():
     while command != "QUIT":
         command = input("Command: ")
         command = command.upper()
+
         if command == "CREATE":
             name = input("Enter a task name: ")
             description = input("Enter a description: ")
             createTask(name, description)
+
         elif command == "TASKS":
+            if getAmountOfTasks() <= 0:
+                print("No tasks")
+                continue
             printTasks()
+
+        elif command == "REMOVE":
+            if getAmountOfTasks() <= 0:
+                print("No tasks to remove")
+                continue
+            taskID = input("Enter Task Number: ")
+            removeTask(taskID)
+
+        elif command == "QUIT":
+            print("Quitting...")
+
         else:
             print("Unknown Command " + command)
 
--- taskSystem.py
+++ taskSystem.py
@@ -1,18 +1,28 @@
 # Handles the tasks, and commands
 from task import *
 
-tasks = []
+tasks = {}
 
 
 def createTask(name, description):
     newTaskID = len(tasks) + 1
     newTask = task(name, description, newTaskID)
-    tasks.append(newTask)
+    tasks.update({str(newTaskID): newTask})
 
 
 def printTasks():
     for i in tasks:
         print(i)
+    print(tasks)
 
 
+def removeTask(taskID):
+    try:
+        tasks.pop(str(taskID))
+    except :
+        print("Task " + taskID + " does not exist")
+
+
+def getAmountOfTasks():
+    return len(tasks)
 # Creating Exceptions
